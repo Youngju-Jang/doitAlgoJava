c34cb7ac94eff0dc698aa22a6076f6722f74148f
Youngju Jang <joj1043@nate.com>
2023-04-12 00:31:11 +0900

    #2 ~16p

--- src/prac/chap01/Max3.java
+++ src/prac/chap01/Max3.java
@@ -7,7 +7,7 @@ class Max3 {
         Scanner stdIn = new Scanner(System.in);
 
         System.out.println("세 정수의 최댓값을 구합니다.");
-        System.out.print("a의 값 : ");  int a = stdIn.nextInt();
+        System.out.print("a의 값 : ");  int a = stdIn.nextInt(); // 키보드 입력값 중 정숫값을 읽어들임
         System.out.print("b의 값 : ");  int b = stdIn.nextInt();
         System.out.print("c의 값 : ");  int c = stdIn.nextInt();
         int max = a;
